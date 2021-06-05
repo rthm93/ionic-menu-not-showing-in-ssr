88294149050935fa393369f3f12ea359f379e6ea
Ryan Teh <srwog@live.com>
2021-06-05 11:52:45 +0800

    Add reproduction steps

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Reproduction Step
+
+1. Run `ng serve`, the ion-menu will be shown as expected
+2. Now run `npm run dev:ssr` ion-menu is not showing
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  constructor(private menuController: MenuController) {}
 
-  constructor() {}
-
+  ngOnInit(): void {
+    this.menuController.open('hardwares');
+  }
 }
